627f211293d6dc23abcd1d1f9546eef8e7c79df3
Evelyn Cowgill <schoonmakere@gmail.com>
2024-10-12 11:53:04 -0500

    Created fragments that have different layout, between tablet and cellphone

--- src/main/java/com/zybooks/thebanddatabase/DetailFragment.java
+++ src/main/java/com/zybooks/thebanddatabase/DetailFragment.java
@@ -7,9 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
 public class DetailFragment extends Fragment {
 
     private Band mBand;
+    public static final String ARG_BAND_ID = "band_id";
 
     public DetailFragment() {
         // Required empty public constructor
@@ -20,6 +22,12 @@ public class DetailFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         int bandId = 1;
+
+        // Get the band ID from the fragment arguments
+        Bundle args = getArguments();
+        if (args != null) {
+            bandId = args.getInt(ARG_BAND_ID);
+        }
 
         // Get the selected band
         mBand = BandRepository.getInstance(requireContext()).getBand(bandId);
